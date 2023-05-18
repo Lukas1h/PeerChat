70e31b318105e4357b90824894b0afb5ca9f6e09
Lukas1h <Lukas1h7@gmail.com>
2023-05-18 16:11:45 -0700

    Initial Commit

---
+++ PeerSotial/PeerSotialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeerSotialApp.swift
+//  PeerSotial
+//
+//  Created by Lukas Hahn on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PeerSotialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
